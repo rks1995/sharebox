e2a0b55c0926111b23f19900eebd84baaf84fa3a
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-02-21 21:19:26 +0530

    use mongoose populate to populate the user

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,13 +1,6 @@
 const Post = require('../models/post');
 
 home = function (req, res) {
-  // Post.find({}, function (err, posts) {
-  //   return res.render('home', {
-  //     title: 'home',
-  //     posts: posts,
-  //   });
-  // });
-
   //populate the user
   Post.find({})
     .populate('user')
